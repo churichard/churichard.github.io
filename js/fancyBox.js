460a212474a5b6f1af86d6be6294580a7b9d2804
Richard Chu <richardchu10@gmail.com>
2014-10-30 15:43:48 -0400

    Added fancyBox and screenshots to Duel

---
+++ js/fancyBox.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic',
+
+		helpers : {
+    		title : {
+    			type : 'inside'
+    		}
+    	}
+	});
+});
